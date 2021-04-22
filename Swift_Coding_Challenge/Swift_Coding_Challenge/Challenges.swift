9b41433d96b8a76ffcbb8d50af4255ca6b65f73d
Luana Chen <luanachen@gmail.com>
2021-04-22 11:17:36 -0300

    Adds isPalindrome

--- Swift_Coding_Challenge/Swift_Coding_Challenge/Challenges.swift
+++ Swift_Coding_Challenge/Swift_Coding_Challenge/Challenges.swift
@@ -9,6 +9,9 @@ import Foundation
 
 class Challenges {
 
+    // ------- STRINGS ------- //
+
+    // UNIQUE LETTERS
     static func hasUniqueLetters(_ string: String) -> Bool {
         var myString = ""
 
@@ -21,4 +24,30 @@ class Challenges {
         }
         return true
     }
+
+    // For best perfomance use Set
+    // Sets cannot contain duplicate items, so if we create a set from the input string then the count of the set will equal the count of the input if there are no duplicates.
+
+    static func hasUniqueLettersWithSet(_ string: String) -> Bool {
+        return Set(string).count == string.count
+    }
+
+    // PALINDROME
+//    static func isPalindrome(_ string: String) -> Bool {
+//        return string.lowercased() == String(string.reversed()).lowercased()
+//    }
+
+    // or
+
+    static func isPalindrome(_ string: String) -> Bool {
+        let lowercase = string.lowercased()
+        return lowercase.reversed() == Array(lowercase)
+    }
+
+    //-----------------------------------------------------------------//
+
 }
+
+
+
+
--- Swift_Coding_Challenge/Swift_Coding_ChallengeTests/Swift_Coding_ChallengeTests.swift
+++ Swift_Coding_Challenge/Swift_Coding_ChallengeTests/Swift_Coding_ChallengeTests.swift
@@ -10,11 +10,29 @@ import XCTest
 
 class Swift_Coding_ChallengeTests: XCTestCase {
 
+    // ------- STRINGS ------- //
+
+    // UNIQUE LETTERS
     func test_uniqueLetters() {
         XCTAssertTrue(Challenges.hasUniqueLetters("no duplicates"))
         XCTAssertTrue(Challenges.hasUniqueLetters("abcdefghijklmnopqrstuvwxyz"))
         XCTAssertTrue(Challenges.hasUniqueLetters("AaBbCc"))
         XCTAssertFalse(Challenges.hasUniqueLetters("Hello, world"))
+    }
+
+    func test_hasUniqueLettersWithSet() {
+        XCTAssertTrue(Challenges.hasUniqueLettersWithSet("no duplicates"))
+        XCTAssertTrue(Challenges.hasUniqueLettersWithSet("abcdefghijklmnopqrstuvwxyz"))
+        XCTAssertTrue(Challenges.hasUniqueLettersWithSet("AaBbCc"))
+        XCTAssertFalse(Challenges.hasUniqueLettersWithSet("Hello, world"))
+    }
+
+    // PALINDROME
+    func test_isPalindrome() {
+        XCTAssertTrue(Challenges.isPalindrome("rotator"))
+        XCTAssertTrue(Challenges.isPalindrome("Rats live on no evil star"))
+        XCTAssertFalse(Challenges.isPalindrome("Never odd or even"))
+        XCTAssertFalse(Challenges.isPalindrome("Hello, world"))
     }
 
 }
